db7fafd3a5d51f65cc145c2ae0e619d74ed8b3ed
Wendy <ckky123@gmail.com>
2023-12-06 22:29:47 +0000

    Failing Test, Called with another value

--- fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_GenerateSingle.cs
+++ fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_GenerateSingle.cs
@@ -5,12 +5,21 @@ namespace FizzBuzz.UnitTests.Services{
     public class FizzBuzzService_GenerateSingle
     {
         [Fact]
-        public void GenerateSingle_ShouldReturnStringWithoutErrors()
+        public void GenerateSingle_ShouldReturn1_WhenCalledWith1()
         {
             var FizzBuzz = new FizzBuzzService();
 
             var result = FizzBuzz.GenerateSingle(1);
             Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void GenerateSingle_ShouldReturn2_WhenCalledWith2()
+        {
+            var FizzBuzz = new FizzBuzzService();
+
+            var result = FizzBuzz.GenerateSingle(2);
+            Assert.Equal("2", result);
         }
     }
 }
