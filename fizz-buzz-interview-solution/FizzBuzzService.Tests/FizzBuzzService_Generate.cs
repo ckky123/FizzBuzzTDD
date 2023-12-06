944da8e6b706456aa8a462102646b8ee2487a4c1
Wendy <ckky123@gmail.com>
2023-12-06 23:23:20 +0000

    Failing Test, Generate for two values

--- fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_Generate.cs
+++ fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_Generate.cs
@@ -4,12 +4,21 @@ namespace FizzBuzz.UnitTests.Services{
     public class FizzBuzzService_Generate
     {
         [Fact]
-        public void Generate_ShouldReturnSingleValue()
+        public void Generate_ShouldReturnSingleString()
         {
             var FizzBuzz = new FizzBuzzService();
 
             var result = FizzBuzz.Generate(1, 1);
             Assert.Equal(1, result.Count);
+        }
+
+        [Fact]
+        public void Generate_ShouldReturnTwoStrings()
+        {
+            var FizzBuzz = new FizzBuzzService();
+
+            var result = FizzBuzz.Generate(1, 2);
+            Assert.Equal(2, result.Count);
         }
     }
 }
