64b550bac7c0293ba094cb9783fe03ac599e2e21
Wendy <ckky123@gmail.com>
2023-12-06 22:59:01 +0000

    Refactor, Create interface for rules

--- fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
+++ fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 
 namespace FizzBuzz.Services{
-    internal class FizzRule
+    public interface IGeneralFizzBuzzRule
+    {
+        bool Check(int number);
+        string Print();
+    }
+
+    internal class FizzRule : IGeneralFizzBuzzRule
     {
         public bool Check(int number) => number % 3 == 0;
         public string Print() => "Fizz";
     }
 
-    internal class BuzzRule
+    internal class BuzzRule : IGeneralFizzBuzzRule
     {
         public bool Check(int number) => number % 5 == 0;
         public string Print() => "Buzz";
     }
     
-    internal class FizzBuzzRule
+    internal class FizzBuzzRule : IGeneralFizzBuzzRule
     {
         public bool Check(int number) => number % 15 == 0;
         public string Print() => "FizzBuzz";
