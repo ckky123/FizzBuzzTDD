5e01c0a8f784b29eebd2ad0c89d0efae600ef7ba
Wendy <ckky123@gmail.com>
2023-12-06 22:26:41 +0000

    Failing Test, GenerateSingle on a simple value returns the correct string

--- fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_GenerateSingle.cs
+++ fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_GenerateSingle.cs
@@ -1,4 +1,5 @@
 using FizzBuzz.Services;
+using Xunit;
 
 namespace FizzBuzz.UnitTests.Services{
     public class FizzBuzzService_GenerateSingle
@@ -9,6 +10,7 @@ namespace FizzBuzz.UnitTests.Services{
             var FizzBuzz = new FizzBuzzService();
 
             var result = FizzBuzz.GenerateSingle(1);
+            Assert.Equal(result, "1");
         }
     }
 }
