18bf3e4e4bf3390e84c087410483fcf032e501d4
Wendy <ckky123@gmail.com>
2023-12-06 23:32:58 +0000

    Add a more comprehensive test

--- fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_Generate.cs
+++ fizz-buzz-interview-solution/FizzBuzzService.Tests/FizzBuzzService_Generate.cs
@@ -22,5 +22,20 @@ namespace FizzBuzz.UnitTests.Services{
             Assert.Equal("1", result[0]);
             Assert.Equal("2", result[1]);
         }
+
+        [Fact]
+        public void Generate_ShouldReturnCorrectStringsForRange()
+        {
+            var FizzBuzz = new FizzBuzzService();
+
+            var result = FizzBuzz.Generate(12, 18);
+            Assert.Equal("Fizz", result[0]);
+            Assert.Equal("13", result[1]);
+            Assert.Equal("14", result[2]);
+            Assert.Equal("FizzBuzz", result[3]);
+            Assert.Equal("16", result[4]);
+            Assert.Equal("17", result[5]);
+            Assert.Equal("Fizz", result[6]);
+        }
     }
 }
