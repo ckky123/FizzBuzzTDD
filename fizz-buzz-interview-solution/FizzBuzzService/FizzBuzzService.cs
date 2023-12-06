117f9a6d4e84a06c8eec45d62b44b890bf3d9a4a
Wendy <ckky123@gmail.com>
2023-12-06 22:38:16 +0000

    Passing Test, Check modulo 5

--- fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
+++ fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
@@ -11,7 +11,7 @@ namespace FizzBuzz.Services{
             if (number % 3 == 0){
                 return "Fizz";
             }
-            else if(number == 5){
+            else if(number % 5 == 0){
                 return "Buzz";
             }
             else{
