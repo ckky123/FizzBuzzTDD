0881090db2a54505aaf01fa3aa86062e34e3de36
Wendy <ckky123@gmail.com>
2023-12-06 22:36:39 +0000

    Passing Test, hardcheck for 5

--- fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
+++ fizz-buzz-interview-solution/FizzBuzzService/FizzBuzzService.cs
@@ -11,6 +11,9 @@ namespace FizzBuzz.Services{
             if (number % 3 == 0){
                 return "Fizz";
             }
+            else if(number == 5){
+                return "Buzz";
+            }
             else{
                 return number.ToString();
             }
